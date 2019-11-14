c993ae688f01872ba7e269c3fff2489083f51700
Josh <joshmh7128@gmail.com>
2019-11-13 20:22:15 -0500

    Added first camera timeline and menu

---
+++ Assets/OnClickStartScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnClickStartScene : MonoBehaviour
+{
+    public Button thisButton;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisButton.onClick.AddListener(LoadScene);   
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
